01f052118b0c9f0084868f29db5043a525737f8a
bibabibu369 <fedorra.2004@gmail.com>
2024-09-25 22:59:30 +0700

    Done Category

--- btth02/index.php
+++ btth02/index.php
@@ -1,20 +1,35 @@
 <?php
-// B1: Bắt giá trị controller và action
+// Khởi động phiên
+session_start();
+
+// Lấy controller và action từ query string
 $controller = isset($_GET['controller']) ? $_GET['controller'] : 'home';
-$action     = isset($_GET['action']) ? $_GET['action'] : 'index';
+$action = isset($_GET['action']) ? $_GET['action'] : 'index';
 
-// B2: Chuẩn hóa tên trước khi gọi
-$controller = ucfirst($controller);
-$controller .= 'Controller';
-$controllerPath = 'controllers/' . $controller . '.php';
+// Tạo tên file controller
+$controllerFile = 'controllers/' . ucfirst($controller) . 'Controller.php';
 
-// B3. Để gọi nó Controller
-if (!file_exists($controllerPath)) {
-    die('Lỗi! Controller này không tồn tại');
+// Kiểm tra nếu file controller tồn tại
+if (file_exists($controllerFile)) {
+    require_once $controllerFile;
+
+    // Tạo tên class controller
+    $controllerClass = ucfirst($controller) . 'Controller';
+
+    // Kiểm tra nếu class controller tồn tại
+    if (class_exists($controllerClass)) {
+        $controllerObject = new $controllerClass();
+
+        // Kiểm tra nếu phương thức action tồn tại
+        if (method_exists($controllerObject, $action)) {
+            $controllerObject->$action();
+        } else {
+            die("Phương thức $action không tồn tại trong controller $controllerClass.");
+        }
+    } else {
+        die("Class controller $controllerClass không tồn tại.");
+    }
+} else {
+    die("File controller $controllerFile không tồn tại.");
 }
-require_once($controllerPath);
-
-// B4. Tạo đối tượng và gọi hàm của Controller
-$myObj = new $controller();  //controller=home > new HomeController()
-$myObj->$action(); //action=index > index()
 ?>
